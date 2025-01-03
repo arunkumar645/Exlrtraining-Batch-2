68310ee5a7812cfc551f57dfa978da9bc37881dc
arunkumar645 <arunkumarterala@gmail.com>
2025-01-03 10:15:00 +0530

    Add files via upload
    Valid credentials using while loop

---
+++ Exlr-training-Batch-2/Demo6.java
@@ -0,0 +1,13 @@
+public class Demo6 {
+    public static void main(String[] args) {
+        String Username;
+        String Password;
+        Username = "Arun";
+        Password = "Arun@123";
+        while ((Username == "Arun") && (Password == "Arun@123")) {
+            System.out.println("Happy new Year");
+            break;
+        }
+
+    }
+}
