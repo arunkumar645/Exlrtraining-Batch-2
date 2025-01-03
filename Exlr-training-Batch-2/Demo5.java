4c251d4d51ae4897aefe7e034b8d9bdce5f94865
arunkumar645 <arunkumarterala@gmail.com>
2025-01-03 10:05:01 +0530

    Login Credentials using while loop

---
+++ Exlr-training-Batch-2/Demo5.java
@@ -0,0 +1,14 @@
+public class Demo5 {
+    private static final String User_001 = null;
+
+    public static void main(String[] args) {
+        String Username;
+        String Password;
+        Username = User_001;
+        Password = User_001;
+        while (Username == Password) {
+            System.out.println("Happy new Year");
+            break;
+        }
+    }
+}
